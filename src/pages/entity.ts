62580518ab9574c338ce1e4fa57f90876d6acf82
Kamaal Farah <kamaal.f1@gmail.com>
2019-08-06 15:28:32 +0200

    Added authentication to post requests

--- src/pages/entity.ts
+++ src/pages/entity.ts
@@ -1,3 +1,4 @@
+import { IsString, Length, MinLength } from "class-validator"
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
 import { BaseEntity } from "typeorm/repository/BaseEntity"
 
@@ -6,9 +7,13 @@ export default class Page extends BaseEntity {
   @PrimaryGeneratedColumn()
   public id?: number
 
+  @IsString()
+  @Length(5, 25)
   @Column("text", { nullable: false })
   public title: string
 
+  @IsString()
+  @MinLength(10)
   @Column("text", { nullable: false })
   public content: string
 }
--- src/users/entity.ts
+++ src/users/entity.ts
@@ -1,3 +1,4 @@
+import { IsEmail, IsString, MinLength } from "class-validator"
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
 import { BaseEntity } from "typeorm/repository/BaseEntity"
 
@@ -6,15 +7,22 @@ export default class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   public id?: number
 
+  @IsString()
+  @MinLength(2)
   @Column("text", { nullable: false })
   public firstName: string
 
+  @IsString()
+  @MinLength(2)
   @Column("text", { nullable: false })
   public lastName: string
 
+  @IsEmail()
   @Column("text", { nullable: false })
   public email: string
 
+  @IsString()
+  @MinLength(3)
   @Column("text")
   public city: string
 }
